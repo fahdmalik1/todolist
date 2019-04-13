6b518e7f9ad75c22567e813ed332c6180f9bd96a
fahdmalik1 <39497356+fahdmalik1@users.noreply.github.com>
2019-04-13 13:09:57 +0100

    array added with object and foreach
    array added with object and foreach

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 var taskInput = document.getElementById("new-task");
 var inputButton = document.getElementById("add-task");
 var divContainer = document.getElementById("list-container");
+
+inputButton.addEventListner("click", function(){
+	addNewTask();
+})
+
+var inputArray = [];
+
+function addNewTask() {
+	
+	var date = new Date();
+	
+	var task = taskInput.value;
+	
+	var todo = {
+		id : inputArray.length + 1,
+		task : task,
+		date : date.toUTCString(),
+		done : false
+	};
+	
+	inputArray.push(todo.task);
+	inputArray.forEach(function(item){
+		list-container.appendChild("<p>" + item + "</p>");
+	})
+	taskInput.value = "";
+	
+	
+}
