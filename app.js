2a9124602602b3d52b023388c3514e6a4d76110f
fahdmalik1 <39497356+fahdmalik1@users.noreply.github.com>
2019-04-13 16:01:34 +0100

    checked to see if input empty
    checking to see if input empty

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var addButton = document.getElementById("new-task-button");
 var listArea = document.getElementById("list-container");
 
 addButton.addEventListener("click", function(){
+	if (!taskInput.value) {
+		return;
+	}
+
+		
 	addNewTask();
 	console.log("eventadded");
+	
 })
 
 var todoArray = [];
@@ -36,11 +42,19 @@ function addNewTask() {
 
 function formatTodo(item) {
 	var listItem = document.createElement('li');
-	var itemContent = document.createElement('span');
-	var taskContent = document.createTextNode(item.task);
 	
-	itemContent.appendChild(taskContent);
-	listItem.appendChild(itemContent);
+	var itemOne = document.createElement('span');
+	var itemTwo = document.createElement('span');
+	
+	var taskContent = document.createTextNode(item.task);
+	var dateContent = document.createTextNode(item.date);
+	
+	
+	itemOne.appendChild(taskContent);
+	itemTwo.appendChild(dateContent);
+	
+	listItem.appendChild(itemOne);
+	listItem.appendChild(itemTwo);
 	listItem.setAttribute('id', item.id);
 	
 	return listItem;
